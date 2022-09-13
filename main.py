2d519aeed33ca547a1231496b3064a4e16802989
André Salvador <andrersalvador@hotmail.com>
2022-09-13 17:15:58 -0300

    Menu de acesso rapido

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+Esse programa será um menu para que seja possivel acessar todos os outros programas de forma mais facil
+inicialmente colocarei apenas os arquivos dos jogos
+'''
+
+print('-=' * 15)
+print(f'{"Bem-Vindo ao menu":^30}')
+print('-=' * 15)
