a8a1d934ab5341c7fb0d90bdd9659d950083d604
André Salvador <andrersalvador@hotmail.com>
2022-10-05 14:13:27 -0300

    update

--- Cardapio/cardapio.py
+++ Cardapio/cardapio.py
@@ -4,67 +4,54 @@ from PIL import Image
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
-
-dic = {}
+pedido = {}
 pedido_total = []
+
+#Colocando em dicionario ao inves de if e else
+cardapio = [
+    {'nome': 'Antartica', 'valor': 6.00},
+    {'nome': 'Skol', 'valor': 6.50},
+    {'nome': 'Brahma', 'valor': 8.20},
+    {'nome': 'Sol', 'valor': 8.25},
+    {'nome': 'Nortenha', 'valor': 16.00},
+    {'nome': 'Proibida', 'valor': 4.80},
+    {'nome': 'Devassa', 'valor': 5.90},
+    {'nome': 'Heineken', 'valor': 9.00},
+]
 
 #Escolha da cerveja
 while True:
-    dic.clear()
+    pedido.clear()
     os.system('cls') or None
 
+    #Print do menu
     print('Escolha sua cerveja: ')
-    print (' 1-ANTARTICA R$6.00;\n 2-SKOL R$6.50;\n 3-BRAHMA R$8.20;\n 4-SOL R$8.25;\n 5-NORTENHA R$16.80;\n 6-PROIBIDA R$4.80;')
-    print(' 7-DEVASSA R$5.90;\n 8-HEINEKEN R$9.00')
-           
+    print('-=' * 17)
+    for v, i in enumerate(cardapio):
+        print(f'Cod: {v} {i["nome"]:.<15} R$ {i["valor"]:.2f}')
+
+    print('-=' * 17)
+
+    #verificação da escolha
     while True:
         beer = int(input("Número da Cerveja: "))
 
-        if beer > 8:
-            print('Digite um número entre 1 à 8')
+        if beer > 7:
+            print('Digite um número entre 0 e 7')
         else:
             break
 
-    quantidade = int(input('Quantas cervejas você deseja: '))
+    quantidade = int(input('Quantas cervejas você deseja: '))  #Pegando informação da quantidade que o cliente deseja
 
-    if beer == 1:
-        valor = float(6 * quantidade)
-        nome = 'Antartica'
-    
-    elif beer == 2:
-        valor = float(6.50 * quantidade)
-        nome = 'Skol'
-    
-    elif beer == 3:
-        valor = float(8.20 * quantidade)
-        nome = 'Brahma'
-    
-    elif beer == 4:
-        valor = float(8.25 * quantidade)
-        nome = 'Sol'
-    
-    elif beer == 5:
-        valor = float(16 * quantidade)
-        nome = 'Nortenha'
-    
-    elif beer == 6:
-        valor = float(4.80 * quantidade)
-        nome = 'Proibida'
-    
-    elif beer == 7:
-        valor = float(5.90 * quantidade)
-        nome = 'Devassa'
-    
-    elif beer == 8:
-        valor = float(9 * quantidade)
-        nome = 'Heineken'
-    
-    dic['nome'] = nome
-    dic['valor'] = valor
-    dic['quantidade'] = quantidade
-    pedido_total.append(dic.copy())
+    pedido['nome'] = cardapio[beer]['nome']
+    pedido['valor'] = cardapio[beer]['valor']
+    pedido['quantidade'] = quantidade
+    pedido['valor'] = pedido['valor'] * pedido['quantidade']  #pegando valor total
 
-    while True:  #verificação do pedido
+    pedido_total.append(pedido.copy())  #jogando tudo em um pedido final
+
+    #verificação do pedido
+    while True:
         escolha = str(input('Deseja continuar[S/N]: ')).lower()[0]
         if escolha in 'sn':
             break
@@ -73,26 +60,30 @@ while True:
     if escolha == 'n':
         break
 
-os.system('cls') or None
+os.system('cls') or None  #Limpesa da tela para que seja possivel ver os valores sem muita poluição
 
 
 #Tratativa dos itens
 total_comprado = 0
 
+#mostrando os valores de forma separada
 for item in pedido_total:
     print(f'Você comprou {item["quantidade"]} {item["nome"]} e ficou R${item["valor"]:.2f}')
     print('=-' * 30)
 
     total_comprado += item['valor']
 
+#valor total da compra
 print(f'O valor total foi de R${total_comprado:.2f}')
 
 print()
+#Métodos de pagamento
 print(' 1- Cartão\n 2- Dinheiro\n 3- Pix\n 4- Boleto')
 
 pagamento = int(input('Qual a forma de pagamento? '))
 
 while True:
+    #Cartão
     if pagamento == 1:
         while True:    
                 dividir = int(input('Quantas vezes deseja fazer: '))
@@ -108,17 +99,20 @@ while True:
                 elif dividir == 0:
                     print('Digite um número igual ou maior que 1!!')
 
+    #Dinheiro
     elif pagamento == 2:
         print(f'Você ganhou um desconto de 5% o total ficou R${total_comprado - (total_comprado * 0.05)}')
         break
 
+    #Pix
     elif pagamento == 3:
         pix_qrcode = qrcode.make('chave do pix')
         pix_qrcode.save('pix.png')
         pix = Image.open('pix.png')
         pix.show()
         break
-
+    
+    #Boleto
     elif pagamento == 4:
         codigo = '0123456789012'
 
