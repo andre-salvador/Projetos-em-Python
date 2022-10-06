1ebf3be7792019df8d967a14134c47808fd1e3b5
André Salvador <andrersalvador@hotmail.com>
2022-10-06 10:54:01 -0300

    Update cardapio.py

--- Cardapio/cardapio.py
+++ Cardapio/cardapio.py
@@ -4,7 +4,7 @@ from PIL import Image
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
-pedido = {}
+
 pedido_total = []
 
 #Colocando em dicionario ao inves de if e else
@@ -21,7 +21,6 @@ cardapio = [
 
 #Escolha da cerveja
 while True:
-    pedido.clear()
     os.system('cls') or None
 
     #Print do menu
@@ -43,12 +42,12 @@ while True:
 
     quantidade = int(input('Quantas cervejas você deseja: '))  #Pegando informação da quantidade que o cliente deseja
 
-    pedido['nome'] = cardapio[beer]['nome']
-    pedido['valor'] = cardapio[beer]['valor']
+    pedido = cardapio[beer].copy()
     pedido['quantidade'] = quantidade
     pedido['valor'] = pedido['valor'] * pedido['quantidade']  #pegando valor total
 
     pedido_total.append(pedido.copy())  #jogando tudo em um pedido final
+    pedido.clear()
 
     #verificação do pedido
     while True:
@@ -80,24 +79,28 @@ print()
 #Métodos de pagamento
 print(' 1- Cartão\n 2- Dinheiro\n 3- Pix\n 4- Boleto')
 
-pagamento = int(input('Qual a forma de pagamento? '))
-
 while True:
+    pagamento = int(input('Qual a forma de pagamento? '))
+
+    if pagamento <= 0 or pagamento > 4:
+        print('Digite um número entre 1 e 4!!')
+
     #Cartão
-    if pagamento == 1:
+    elif pagamento == 1:
         while True:    
-                dividir = int(input('Quantas vezes deseja fazer: '))
+            dividir = int(input('Quantas vezes deseja fazer: '))
 
-                if dividir == 1:
-                    print(f'O valor total ficou R${total_comprado}')
-                    break
+            if dividir == 1:
+                print(f'O valor total ficou R${total_comprado}')
+                break
 
-                elif dividir > 1:
-                    print(f'Você escolheu dividir em {dividir} vezes, ficou no total de {total_comprado / dividir}')
-                    break
+            elif dividir > 1:
+                print(f'Você escolheu dividir em {dividir} vezes, ficou no total de R${total_comprado / dividir:.2f}')
+                break
 
-                elif dividir == 0:
-                    print('Digite um número igual ou maior que 1!!')
+            else:
+                print('Digite um número igual ou maior que 1!!')
+        break
 
     #Dinheiro
     elif pagamento == 2:
